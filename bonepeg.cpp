641b1fe133c1057f2e9d8f025159a0d21877e124
rgm <git.rgm@spamgourmet.com>
2013-07-08 00:47:18 -0500

    Support between 2 and 26 levels of grey in terminal preview.

--- bonepeg.cpp
+++ bonepeg.cpp
@@ -1,15 +1,19 @@
 /* bonepeg.cpp
  *
- * Based on boneCV[1] by Derek Molloy, and his handy video [2].
+ * A program to stream video from a webcam connected to the BeagleBone to a Peggy 2.
+ * With in-terminal preview, ASCII-cam style.
+ * For now, needs 256-color capable terminal.
+ *
+ * Based on boneCV [1] by Derek Molloy, and his handy video [2].
  * Ascii character palettes from Paul Bourke [3].
  *
- * 1 = boneCV, https://github.com/derekmolloy/boneCV
+ * [1]: boneCV, https://github.com/derekmolloy/boneCV
  *
- * 2 = Molloy, D. [DerekMolloyDCU]. "Beaglebone: Video Capture and Image Processing on Embedded Linux using OpenCV [Video file]."
- *     25 May 2013. Youtube. 7 July 2013. <http://www.youtube.com/watch?v=8QouvYMfmQo>
+ * [2]: Molloy, D. [DerekMolloyDCU]. "Beaglebone: Video Capture and Image Processing on Embedded Linux using OpenCV [Video file]."
+ *      25 May 2013. Youtube. 7 July 2013. <http://www.youtube.com/watch?v=8QouvYMfmQo>
  *
- * 3 = Bourke, Paul. "Character representation of greyscale images"
- *     Feb. 1997. Web. 8 July 2013.  <http://paulbourke.net/dataformats/asciiart/>
+ * [3]: Bourke, Paul. "Character representation of greyscale images"
+ *      Feb. 1997. Web. 8 July 2013.  <http://paulbourke.net/dataformats/asciiart/>
  *     
  */
 
@@ -19,6 +23,7 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <locale.h>
+//#include <ncurses.h>
 
 // Capture size from camera
 #define WIDTH 800
@@ -29,13 +34,22 @@ using namespace cv;
 
 // Declared functions
 void printThumbnail(Mat thumb);
-
+uint8_t grey82ansi(uint8_t grey8);
+uint8_t grey82ansi(uint8_t grey8, uint8_t paletteSize);
 
 // Globals
-Size peggySize = Size(25,25);
+Size peggySize = Size(25, 25);
 
+/*
+ * signal handler
+ */
 void my_handler(int s) {
+    //endwin(); TODO ncurses
+    printf("\x1B[?25h"); // show cursor
+
+    // print message and restore prompt under video
     printf("\x1B[0m\x1B[%d;1H\nCaught signal %d\n", peggySize.height, s);
+
     exit(1); 
 }
 
@@ -49,17 +63,20 @@ int main(int argc, char** argv)
     sigaction(SIGINT, &sigIntHandler, NULL);
 
     // set locale
-     if (!setlocale(LC_CTYPE, "")) {
-      fprintf(stderr, "Can't set the specified locale! "
-                  "Check LANG, LC_CTYPE, LC_ALL.\n");
-           return 1;
+    if (!setlocale(LC_CTYPE, "")) {
+        // cerr << "Can't set the specified locale! Check LANG, LC_CTYPE, LC_ALL." << endl; // any practical difference?
+        fprintf(stderr, "Can't set the specified locale! "
+                "Check LANG, LC_CTYPE, LC_ALL.\n");
+        return 1;
     }
 
+    // prints "VIDIOC_QUERYMENU: Invalid argument" on stderr a few times with this OpenCV, but works fine
+    // Seems to be an issue with OpenCV querying device capabilities
     VideoCapture capture(0);
     capture.set(CV_CAP_PROP_FRAME_WIDTH, WIDTH);
     capture.set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
 
-    if(!capture.isOpened()){
+    if (!capture.isOpened()) {
         cout << "Failed to connect to the camera." << endl;
     }
 
@@ -67,15 +84,17 @@ int main(int argc, char** argv)
     Mat frame, cropped, grey, thumb;
     Rect squareInCenter((WIDTH - HEIGHT) / 2, 0, HEIGHT, HEIGHT);
     
-    // 16 characters arranged (roughly) in order of increasing density (brightness)
-    char palette16[] = " .'^\":;!i?ZM#%@";
-
-	// These ramps suggested by Paul Bourke [3]
-    //char palette10[] = " .:-=+*#%@";
-	//char palette72[]= "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
     
-    // [2J = Clear screen, [1;1H = [;H = put cursor at upper left (position 1,1)
-    printf("\x1B[2J\x1B[;H");
+    // [2J   = Clear screen
+    // [1;1H = [;H = put cursor at upper left (position 1,1)
+    // [?25l = Hide cursor
+    printf("\x1B[2J\x1B[;H\x1B[?25l");
+
+    //initscr(); //TODO - ncurses
+    //cbreak();
+    //noecho();
+    //start_color();
+    //refresh();
     
     /*
      * Main webcam capture loop
@@ -106,30 +125,110 @@ int main(int argc, char** argv)
 
     }//capture loop
 
+    //endwin(); TODO ncurses
+
     return 0;
 }
 
-// Prints the thumbnail to the terminal
-// assumes utf8 and 256 color support
-// TODO -- detect 256 color, utf8, provide fallbacks
+/* Prints the thumbnail to the terminal
+ * assumes utf8 and 256 color support
+ * TODO -- detect 256 color, utf8, provide fallbacks
+ */
 void printThumbnail(Mat thumb) {
-	int fourBitGrey = 0;
-    int ansiGrey = 0;
-	char utf8boxes[] = "\xE2\x96\x88\xE2\x96\x88";
+    //char utf8boxes[] = "\xE2\x96\x88\xE2\x96\x88"; // two FULL BLOCK U+2588 in utf8
+    // 16 characters arranged (roughly) in order of increasing density (brightness)
+    //const char palette16[] = " .'^\":;!i?ZM#%@";
+    //const std::string palette16 (" .'^\":;!i?ZM#%@");
 
+    // These ramps suggested by Paul Bourke [3]
+    //char palette10[] = " .:-=+*#%@";
+    //char palette72[]= "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+    
+    uint8_t rawGrey = 0, // 0-15
+            ansiGrey = 0,
+            rows = thumb.rows,
+            cols = thumb.cols;
+
+    // options
+    bool mirror = true,
+         utf8 = true,
+         hicolor = true;
 
     // Move cursor to row 1, column 1
     printf("\x1B[;H");
 
-	for(int i = 0; i < thumb.rows; i++ ) {
-		for(int j = 0; j < thumb.cols; j++ ) {
-			fourBitGrey = thumb.at<uchar>(i,j) >> 4; // 0-15
+    for(int i = 0; i < rows; i++ ) {
+        for(int j = 0; j < cols; j++ ) {
 
-			// extended ANSI colors are 0-255.  Black starts at 232, white at 255.
-			ansiGrey = 232 + ((int) (1.43 * fourBitGrey));
-			printf("\x1B[38;5;%dm%s", ansiGrey, utf8boxes);
-		}
+            rawGrey = mirror ? thumb.at<uchar>(i, (cols - 1) - j) : thumb.at<uchar>(i, j);
 
-		printf("\n");
-	}
+            if (hicolor) {
+                // Print two space characters with ansi grey background color
+                // scaled across the 26 grey levels in the default ANSI 256 palette
+                // extended ANSI colors are 0-255. 231 = #FFFFFF, 232 = #080808, 255 = #EEEEEE
+                // To approximate peggy display:  grey82ansi(rawGrey, 16)
+                ansiGrey = grey82ansi(rawGrey);
+                printf("\x1B[48;5;%dm  ", ansiGrey);
+
+                // TODO ncurses
+                //mvprintw(0, j, "\x1B[48;5;%dm  ", ansiGrey);
+                //mvprintw(0, j, "%c", palette16.at(rawGrey >> 4));
+            }
+        }
+
+        printf("\n");
+    }
+    //refresh();
+}
+
+// Converts an 8-bit greyscale value to one of the 26 ANSI grey levels.
+uint8_t grey82ansi(uint8_t grey8) {
+    return grey82ansi(grey8, 26);
+}
+
+// Returns the best ANSI color code for the given 8-bit grey value, within the
+// size of the palette.  26 ANSI grey levels are available, including black and white.
+// color index 16 = black, 231 = white, 232 - 255 dark (#080808) to light (#EEEEEE) ramp 
+//
+// Wanring: the 256 colors in the palette can be redefined.
+//
+// @param grey8         an 8-bit grey value.  Gets scaled to 0-paletteSize.
+// @param paletteSize   The number of greys in the palette.
+uint8_t grey82ansi(uint8_t grey8, uint8_t paletteSize) {
+    const uint8_t MAX_GREYS = 26, MIN_GREYS = 2;
+    const uint8_t ANSIGREYS[MAX_GREYS] = { 16, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 
+                                           244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 231 };
+    uint8_t scaled = 0;
+
+    // bounds check
+    if (paletteSize > MAX_GREYS) { paletteSize = MAX_GREYS; }
+    if (paletteSize < MIN_GREYS) { paletteSize = MIN_GREYS; }
+
+    // if the paletteSize is a power of two [2,4,8,16] we can shift for scale
+    // generic power of two test: paletteSize & (paletteSize - 1)) == 0)
+    // Just hard-code the four supported
+    switch(paletteSize) {
+        case 16:
+            scaled = grey8 >> 4; // 0-15
+            break;
+        case 8:
+            scaled = grey8 >> 5; // 0-7
+            break;
+        case 4:
+            scaled = grey8 >> 6; // 0-4
+            break;
+        case 2:
+            scaled = grey8 >> 7; // 0-1, black or white
+            break;
+        default:
+            scaled = (int)((grey8 * paletteSize) / (255 + 1)); // 0-(paletteSize-1)
+    }
+
+    // If using max greys in palette, the scaled value is equal to the index of the ansi grey color
+    if (paletteSize == MAX_GREYS) {
+        return ANSIGREYS[scaled];
+    }
+
+    int greyIdx = (scaled * (MAX_GREYS - 1)) / (paletteSize - 1);
+    return ANSIGREYS[greyIdx]; 
 }
