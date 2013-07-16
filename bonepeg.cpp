fdc8acec926dfe3dd3e02426d785a4c44f1e1caa
rgm <git.rgm@spamgourmet.com>
2013-07-15 23:22:01 -0500

    Terminal preview uses 256 grey levels

--- bonepeg.cpp
+++ bonepeg.cpp
@@ -29,13 +29,14 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-using namespace std;
+//using namespace std;
 using namespace cv;
 
 // Declared functions
 void printThumbnail(Mat thumb);
 uint8_t grey82ansi(uint8_t grey8);
 uint8_t grey82ansi(uint8_t grey8, uint8_t paletteSize);
+int mapVal(int value, int fromLow, int fromHigh, int toLow, int toHigh);
 
 // Globals
 Size peggySize = Size(25, 25);
@@ -44,10 +45,8 @@ Size peggySize = Size(25, 25);
  * signal handler
  */
 void my_handler(int s) {
-
-    printw("Caught signal %d\n", s);
-
     endwin();
+    printf("Caught signal %d\n", s);
     exit(1); 
 }
 
@@ -62,7 +61,7 @@ int main(int argc, char** argv)
 
     // set locale
     if (!setlocale(LC_CTYPE, "")) {
-        // cerr << "Can't set the specified locale! Check LANG, LC_CTYPE, LC_ALL." << endl; // any practical difference?
+        // std::cerr << "Can't set the specified locale! Check LANG, LC_CTYPE, LC_ALL." << std::endl; // any practical difference?
         fprintf(stderr, "Can't set the specified locale! "
                 "Check LANG, LC_CTYPE, LC_ALL.\n");
         return 1;
@@ -75,7 +74,8 @@ int main(int argc, char** argv)
     capture.set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
 
     if (!capture.isOpened()) {
-        cout << "Failed to connect to the camera." << endl;
+        std::cout << "Failed to connect to the camera." << std::endl;
+        return 2;
     }
 
     // Matrices for captured frame, grey frame, and thumbnail
@@ -87,7 +87,17 @@ int main(int argc, char** argv)
     cbreak();
     noecho();
     start_color();
-    refresh();
+    use_default_colors();
+    curs_set(0);
+
+    // Setup a palette of 256 shades of grey
+    // TODO check COLORS >= 256 && if( can_change_color() )
+    int mapped;
+    for (int i = 0; i < 256; i++) {
+        mapped = mapVal(i, 0, 255, 0, 1000);
+        init_color(i, mapped, mapped, mapped);
+        init_pair(i, COLOR_BLACK, i);
+    }
     
     /*
      * Main webcam capture loop
@@ -98,7 +108,7 @@ int main(int argc, char** argv)
         capture >> frame;
 
         if (frame.empty()) {
-            cout << "Failed to capture an image" << endl;
+            std::cout << "Failed to capture an image" << std::endl;
             return -1;
         }
 
@@ -112,12 +122,14 @@ int main(int argc, char** argv)
         thumb = Mat(peggySize.width, peggySize.height, CV_8U);
         resize(grey, thumb, peggySize, 0, 0, INTER_LANCZOS4);
 
-        //cout << "Thumb = " << endl << thumb << endl << endl; // debug
+        //std::cout << "Thumb = " << std::endl << thumb << std::endl << std::endl; // debug
         printThumbnail(thumb);
+        refresh();
 
 
     }//capture loop
 
+    curs_set(1);
     endwin();
 
     return 0;
@@ -127,13 +139,14 @@ int main(int argc, char** argv)
  */
 void printThumbnail(Mat thumb) {
     // 16 characters arranged (roughly) in order of increasing density (brightness)
-    //const char palette16[] = " .'^\":;!i?ZM&#%@";
-    const std::string palette16 (" .'^\":;!i?ZM&#%@");
+    //const char palette[] = " .'^\":;!i?ZM&#%@";
+    //const string palette16 (" .'^\":;!i?ZM&#%@");
 
     // These ramps suggested by Paul Bourke [3]
-    //char palette10[] = " .:-=+*#%@";
-    //char palette72[]= "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-    
+    //const char palette[] = " .:-=+*#%@";
+    //const char palette[] = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+    //int paletteSize = strlen(palette);
+
     uint8_t rawGrey = 0, // 0-15
             ansiGrey = 0,
             rows = thumb.rows,
@@ -155,13 +168,28 @@ void printThumbnail(Mat thumb) {
                 // extended ANSI colors are 0-255. 231 = #FFFFFF, 232 = #080808, 255 = #EEEEEE
                 // To approximate peggy display:  grey82ansi(rawGrey, 16)
                 //ansiGrey = grey82ansi(rawGrey);
-               
-               	char ch = palette16.at(rawGrey >> 4);
-                mvprintw(i, j*2, "%c%c", ch, ch);
+
+                // index in brightness ramp char array
+                //int idx = mapByte(rawGrey, 0, paletteSize - 1);
+                //char ch = palette[idx];
+                //mvprintw(i, j*2, "%c%c", ch, ch);
+                attron(COLOR_PAIR(rawGrey));
+                mvprintw(i, j*2, "  ");
             }
         }
     }
-    refresh();
+}
+
+// Map a value from one range to another.  Works with negative numbers too.
+// assert(mapVal(500, 0, 1000, 50, 100) == 75)
+// assert(mapVal(2, 0, 10, -100, -200) == -120)
+// assert(mapVal(10, 0, 0, 0, 0) == 0)
+int mapVal(int value, int fromLow, int fromHigh, int toLow, int toHigh) {
+    return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+}
+// Map a uint8_t (0-255) to a new range
+int mapByte(uint8_t value, int low, int high) {
+    return mapVal(value, 0, 255, low, high);
 }
 
 // Converts an 8-bit greyscale value to one of the 26 ANSI grey levels.
@@ -173,8 +201,7 @@ uint8_t grey82ansi(uint8_t grey8) {
 // size of the palette.  26 ANSI grey levels are available, including black and white.
 // color index 16 = black, 231 = white, 232 - 255 dark (#080808) to light (#EEEEEE) ramp 
 //
-// Wanring: the 256 colors in the palette can be redefined.
-//
+// Warning: the 256 colors in the palette can be redefined.
 // @param grey8         an 8-bit grey value.  Gets scaled to 0-paletteSize.
 // @param paletteSize   The number of greys in the palette.
 uint8_t grey82ansi(uint8_t grey8, uint8_t paletteSize) {
@@ -212,6 +239,7 @@ uint8_t grey82ansi(uint8_t grey8, uint8_t paletteSize) {
         return ANSIGREYS[scaled];
     }
 
-    int greyIdx = (scaled * (MAX_GREYS - 1)) / (paletteSize - 1);
+    //int greyIdx = (scaled * (MAX_GREYS - 1)) / (paletteSize - 1);
+    int greyIdx = mapVal(scaled, 0, paletteSize - 1, 0, MAX_GREYS - 1);
     return ANSIGREYS[greyIdx]; 
 }
